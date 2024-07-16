2fc767a9cb13f41f96d83a8a0a3b78384d119db5
piraniMarco <129805834+piraniMarco@users.noreply.github.com>
2024-07-16 11:20:23 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('jobtime_V12', 'root', '', {
+const sequelize = new Sequelize('jobtime_V13', 'jobtime_user', 'Momento@170839', {
     host: 'localhost',
     dialect: 'mysql',
 });
